9eb09c30dceb112fc1daa09658778c9a2017a3b6
Christian Ilagan <cmilagan7@gmail.com>
2021-08-20 03:10:25 +1000

    Fix responsiveness of about page

--- src/components/sections/AboutSection.js
+++ src/components/sections/AboutSection.js
@@ -5,10 +5,13 @@ import me from '../../images/me.JPG'
 function AboutSection() {
     return (
         <div className="about-container">
-                <div className="box">
+            <div className="box">
+                <div className="left">
                     <div className="img-container">
                         <img className='me' src={ me } alt="" />
                     </div>
+                </div>
+                <div className="right">
                     <div className="content">
                         <h1>Hi i'm Christian!</h1>
 
@@ -16,13 +19,12 @@ function AboutSection() {
                             I'm currently in my penultimate year of Computer Science at UNSW. Throughout my
                             career thus far, I have found my self to be extremely interested in web and 
                             game development.
-                        </p>
 
-                        <p>
                             Other than academics I enjoy working out and playing sports such as basketball. I 
                             am also part of the UNSW security society as a media subcommitee member, and engage myself
                             with extracurricular programs such as VIP CHALLENG offered by UNSW.
                         </p>
+                    </div>
                 </div>
             </div>
         </div>
