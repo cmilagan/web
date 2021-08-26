1106902f797180e801133368d15d92d3858917e1
Christian Ilagan <cmilagan7@gmail.com>
2021-08-26 14:59:49 +1000

    Added temporary projects page

---
+++ src/components/sections/ProjectsList.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import CardItem from '../reusables/CardItem'
+import loopmania from '../../images/loopmania.PNG'
+import website from '../../images/personalweb.PNG'
+import stimpact from '../../images/stimpact.jpg'
+import flockr from '../../images/flockr.png'
+import flatpack from '../../images/flatpack.PNG'
+import './ProjectsList.css'
+
+const ProjectsList = () => {
+    return (
+        <div className="projects__container">
+            <ul className="projects__items">
+                <CardItem 
+                    src={loopmania}
+                    text="Videogame created using OOP (Java) and JFX"
+                    label="Game"
+                    path='/web/post/1629640800'
+                />
+                <CardItem 
+                    src={website}
+                    text="Personal website created with HTML/CSS/JavaScript (REACT)"
+                    label="Website"
+                    path='/web/post/1629554400'
+                />
+                <CardItem 
+                    src={stimpact}
+                    text="Developed a neuralstimulator to be used on animals (C/Python/Kivy)"
+                    label="Firmware/Software"
+                    path='/web/post/1629900000'
+                />
+                <CardItem 
+                    src={flockr}
+                    text="Developed a communication platform similar to slack/discord (Python/Flask)"
+                    label="Frontend/Backend"
+                    path='/web/post/1629727200'
+                />
+                <CardItem 
+                    src={flatpack}
+                    text="Developed modular furniture targeting the problem of waste"
+                    label="Design/Development"
+                    path='/web/post/1629813600'
+                />
+            </ul>
+        </div>
+    )
+}
+
+export default ProjectsList
--- src/components/sections/ProjectsSection.js
+++ src/components/sections/ProjectsSection.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './ProjectsSection.css'
-
+import ProjectsList from './ProjectsList'
 function ProjectsSection() {
     return (
-        <div className="projects-container">
-            <h1>
-                PAGE COMING SOON :DDD
-            </h1>
+        <div className="projects__section">
+            <h1 className="title">All Projects</h1>
+            <ProjectsList />
         </div>
+
     )
 }
 
