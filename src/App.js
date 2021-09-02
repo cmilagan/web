d4da4a4a832ec2229bd6fc87f60a8dd44adc0e38
Christian Ilagan <cmilagan7@gmail.com>
2021-09-02 21:19:39 +1000

    Fix routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/pages/Home'
 import Resume from './components/pages/Resume';
 import About from './components/pages/About';
